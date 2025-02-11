3488b875ee2e70ab7fc0e478be3b0cf3be78baec
HanGilKim01 <khg6059@hanyang.ac.kr>
2025-02-11 18:42:29 +0900

    Delete log.py

--- log.py
+++
@@ -1,9 +0,0 @@
-# Log simulation events
-LOG_DAILY_EVENTS = []
-
-# Log daily repots: Inventory level for each item; daily change for each item; Remaining demand (demand - product level)
-LOG_DAILY_REPORTS = []
-LOG_STATE_DICT = []
-
-
-GRAPH_LOG = {}
